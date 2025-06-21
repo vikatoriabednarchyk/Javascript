53e7f327aaa17d8387645a9622fe97d71fa097ff
viktoriabednarchyk <viktoriabednarcik@gmail.com>
2025-06-21 16:07:30 +0300

    cтворили task2_1.html та script.js та внесли зміни

---
+++ Homework/Hw2/task2_1/script.js
@@ -0,0 +1,13 @@
+let arr = [20, false, 'java', NaN, "okten", true, -1, [], null, 'owu']
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);
+console.log(arr[8]);
+console.log(arr[9]);
+
