5a0e1ad8a8152e88a1e9a4ffb55015396662831d
viktoriabednarchyk <viktoriabednarcik@gmail.com>
2025-06-21 17:37:50 +0300

    cтворили task2_8.html та script.js та внесли зміни

---
+++ Homework/Hw2/task2_8/script.js
@@ -0,0 +1,11 @@
+let day = +prompt('Enter number of month')
+
+if (day >=1 && day <10){
+    console.log('first decade');
+}
+else if(day >=10 && day <20){
+    console.log('second decade');
+}
+else if (day >=20 && day <31){
+    console.log('third decade');
+}
