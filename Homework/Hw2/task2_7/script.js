e484c7d88062be95ad8b0c5e841d9e88ac88b9f2
viktoriabednarchyk <viktoriabednarcik@gmail.com>
2025-06-21 17:27:41 +0300

    cтворили task2_7.html та script.js та внесли зміни

---
+++ Homework/Hw2/task2_7/script.js
@@ -0,0 +1,13 @@
+let time = +prompt('Enter hour 0-59');
+
+if (time >=0 && time < 15){
+    console.log('first half');}
+else if (time >= 15 && time < 30){
+    console.log('second half');
+}
+else if (time >= 30 && time < 45){
+    console.log('third half');
+}
+else if (time >= 45 && time < 60){
+    console.log('third half');
+}
