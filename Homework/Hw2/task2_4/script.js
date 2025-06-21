487c7b04ed71f70abbdbf0e520640e439c144096
viktoriabednarchyk <viktoriabednarcik@gmail.com>
2025-06-21 16:41:58 +0300

    cтворили task2_4.html та script.js та внесли зміни

---
+++ Homework/Hw2/task2_4/script.js
@@ -0,0 +1,23 @@
+let users = [
+    {   name: "John", userName: "John23", password: "123456"},
+    {   name: "Mary", userName: "Mary223", password: "qwerty"},
+    {   name: "Anna", userName: "Anna93", password: "H42vt"},
+    {   name: "Jack", userName: "Jack2212", password: "Jack12345"},
+    {   name: "Michel", userName: "Michel4223", password: "g1r2e3w"},
+    {   name: "Julia", userName: "Julia213", password: "11111111"},
+    {   name: "Kate", userName: "Kate2893", password: "565656"},
+    {   name: "Helen", userName: "Helen653", password: "h3kd1"},
+    {   name: "Robert", userName: "Robert2102", password: "334443"},
+    {   name: "Bob", userName: "Bob421", password: "1wf2f3"}
+]
+
+console.log(users[0])
+console.log(users[1])
+console.log(users[2])
+console.log(users[3])
+console.log(users[4])
+console.log(users[5])
+console.log(users[6])
+console.log(users[7])
+console.log(users[8])
+console.log(users[9])
