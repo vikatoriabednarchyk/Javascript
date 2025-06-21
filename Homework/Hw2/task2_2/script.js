c7530902ba1359c716f5e831d13d140b2787a060
viktoriabednarchyk <viktoriabednarcik@gmail.com>
2025-06-21 16:19:28 +0300

    cтворили task2_2.html та script.js та внесли зміни

---
+++ Homework/Hw2/task2_2/script.js
@@ -0,0 +1,18 @@
+let book1 = {
+    title: 'Harry Potter and the Philosophers Stone',
+    pageCount : 352,
+    genre : 'fantasy'
+}
+let book2 = {
+    title: 'Black Arrow',
+    pageCount : 176,
+    genre : 'adventure and historical story'
+}
+let book3 = {
+    title: 'The Watsons',
+    pageCount : 128,
+    genre : 'novel'
+}
+console.log(book1)
+console.log(book2)
+console.log(book3)
