3a5f87c42d1422a5dc6285dbfb2c5e6c883fc7d7
viktoriabednarchyk <viktoriabednarcik@gmail.com>
2025-06-21 16:54:48 +0300

    cтворили task2_5.html та script.js та внесли зміни

---
+++ Homework/Hw2/task2_5/script.js
@@ -0,0 +1,10 @@
+let temps = [
+    {day: 'Monday', morning: 16, dinner: 30, evening: 21},
+    {day: 'Tuesday', morning: 15, dinner: 28, evening: 23},
+    {day: 'Wednesday', morning: 13, dinner: 25, evening: 21},
+    {day: 'Thursday', morning: 10, dinner: 20, evening: 15},
+    {day: 'Friday', morning: 16, dinner: 21, evening: 18},
+    {day: 'Saturday', morning: 18, dinner: 27, evening: 23},
+    {day: 'Sunday', morning: 15, dinner: 22, evening: 16},
+]
+console.log(temps);
