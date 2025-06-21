7bd03a63bd24c85d5f7cc994a44e1fbb7864c494
viktoriabednarchyk <viktoriabednarcik@gmail.com>
2025-06-21 16:30:32 +0300

    cтворили task2_3.html та script.js та внесли зміни

---
+++ Homework/Hw2/task2_3/script.js
@@ -0,0 +1,31 @@
+let book1 = {
+    title: 'Harry Potter and the Philosophers Stone',
+    pageCount : 352,
+    genre : 'fantasy',
+    authors: [
+        {name: 'Robert Galbraith', age: 59},
+        {name: 'Roald Dahl', age: 48}
+    ]
+}
+let book2 = {
+    title: 'Black Arrow',
+    pageCount : 176,
+    genre : 'adventure and historical story',
+    authors: [
+        {name: 'Beatrix Potter', age: 35},
+        {name: 'Dr. Seuss', age: 60}
+]
+}
+let book3 = {
+    title: 'The Watsons',
+    pageCount : 128,
+    genre : 'novel',
+    authors: [
+        {name: 'Julia Donaldson', age: 57},
+        {name: 'Robert Sabuda', age: 42}
+        ]
+}
+
+console.log(book1)
+console.log(book2)
+console.log(book3)
