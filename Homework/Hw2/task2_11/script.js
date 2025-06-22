0e3be22c4741562d2a34e5abdb058ff923b0be0c
viktoriabednarchyk <viktoriabednarcik@gmail.com>
2025-06-22 17:43:31 +0300

    cтворили task2_11.html та script.js та внесли зміни

---
+++ Homework/Hw2/task2_11/script.js
@@ -0,0 +1,17 @@
+let x = '';
+
+if (x === 0){
+    console.log('default');
+}
+else if (x === 'null'){
+    console.log('default');
+}
+else if (x === 'undefined'){
+    console.log('default');
+}
+else if (x === ''){
+    console.log('default');
+}
+else if (x === 'NaN'){
+    console.log('default');
+}
