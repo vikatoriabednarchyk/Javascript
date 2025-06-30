bb83961a034ac8a5649ba11cd8fffa3ec2394475
viktoriabednarchyk <viktoriabednarcik@gmail.com>
2025-06-30 17:49:49 +0300

    cтворили task2_12.html та script.js та внесли зміни

---
+++ Homework/Hw2/task2_12/script.js
@@ -0,0 +1,12 @@
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+if (coursesAndDurationArray >= 5) {
+    console.log('Супер');
+}
