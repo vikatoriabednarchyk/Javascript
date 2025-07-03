320d6925dd88965beb3a8ec8da9a036653913df7
viktoriabednarchyk <viktoriabednarcik@gmail.com>
2025-07-03 13:27:07 +0300

    Створили task3_5.html та script.js та внесли зміни

---
+++ Homework/Hw3/task3_5/script.js
@@ -0,0 +1,6 @@
+let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+document.write(`<ul>`);
+for (const item of listOfItems) {
+    document.write(`<li> ${item} </li>`);
+}
+document.write(`</ul>`);
