104b75b3aa4cfca1f57726e6b97fcac51b46da7c
viktoriabednarchyk <viktoriabednarcik@gmail.com>
2025-07-04 20:10:56 +0300

    Створили task3_9.html та script.js та внесли зміни

---
+++ Homework/Hw3/task3_9(add)/script.js
@@ -0,0 +1,6 @@
+let numbers = [1,2,3,4,5];
+    console.log(numbers);
+let strings = ['hello','tea','rain','sun','apple'];
+    console.log(strings);
+let numbersStringsBooleans = [35, true, 'sun', false, -333]
+    console.log(numbersStringsBooleans);
