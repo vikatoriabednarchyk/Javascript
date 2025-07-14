6cec91559076fba25fd7b00711ba1112d9261277
viktoriabednarchyk <viktoriabednarcik@gmail.com>
2025-07-14 20:11:17 +0300

    Створили task3_18.html та script.js та внесли зміни

---
+++ Homework/Hw3/task3_18(add)/script.js
@@ -0,0 +1,15 @@
+let arr = [];
+arr[0] = 18;
+arr[1] = null;
+arr[2] = 'sun';
+arr[3] = 'rain';
+arr[4] = 100;
+arr[5] = true;
+arr[6] = 934;
+arr[7] = undefined;
+arr[8] = 'ok';
+arr[9] = false;
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
