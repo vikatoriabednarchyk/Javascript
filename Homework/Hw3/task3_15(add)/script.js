ae368ea11b65c87c675ea83937bd4797f57edfdd
viktoriabednarchyk <viktoriabednarcik@gmail.com>
2025-07-14 20:02:58 +0300

    Створили task3_15.html та script.js та внесли зміни

---
+++ Homework/Hw3/task3_15(add)/script.js
@@ -0,0 +1,8 @@
+let arr = [true, false, 1, 2, 3, 'rain', 'sun', 55, -1, 'hello']
+
+for (let i = 0; i < arr.length; i++) {
+  if (typeof arr[i] === 'boolean') {
+      console.log(arr[i]);
+  }
+
+}
