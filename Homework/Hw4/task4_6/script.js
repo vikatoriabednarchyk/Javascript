2bcfe3fd87adfad63ce3d25217587d1d1a151960
viktoriabednarchyk <viktoriabednarcik@gmail.com>
2025-07-21 21:04:38 +0300

    Створили task4_6.html та script.js та внесли зміни

---
+++ Homework/Hw4/task4_6/script.js
@@ -0,0 +1,8 @@
+function foobar(text) {
+    document.write(`<ul> <li>${text}</li>
+                         <li>${text}</li>
+                         <li>${text}</li>
+                    </ul>`);
+}
+
+foobar('hello world');
