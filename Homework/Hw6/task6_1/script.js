1f0e2b0d5de88c28610b306a89f4367a44c5aa8f
viktoriabednarchyk <viktoriabednarcik@gmail.com>
2025-08-19 16:51:04 +0300

    Створили task6_1.html та script.js та внесли зміни

---
+++ Homework/Hw6/task6_1/script.js
@@ -0,0 +1,7 @@
+let string1 = 'hello world';
+let string2 = 'lorem ipsum';
+let string3 = 'javascript is cool';
+
+console.log(string1.length);
+console.log(string2.length);
+console.log(string3.length);
