70f19bffc2ab02f17ff657c385363233d05b8143
viktoriabednarchyk <viktoriabednarcik@gmail.com>
2025-08-19 16:56:07 +0300

    Створили task6_3.html та script.js та внесли зміни

---
+++ Homework/Hw6/task6_3/script.js
@@ -0,0 +1,7 @@
+let string1 = 'HELLO WORD';
+let string2 = 'LOREM IPSUM';
+let string3 = 'JAVASCRIPT IS COOL   ';
+
+console.log(string1.toLowerCase());
+console.log(string2.toLowerCase());
+console.log(string3.toLowerCase());
