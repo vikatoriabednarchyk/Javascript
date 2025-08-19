6a6a99c4b02048d5638da319eabc7aa3c73a09f7
viktoriabednarchyk <viktoriabednarcik@gmail.com>
2025-08-19 16:53:29 +0300

    Створили task6_2.html та script.js та внесли зміни

---
+++ Homework/Hw6/task6_2/script.js
@@ -0,0 +1,7 @@
+let string1 = 'hello world';
+let string2 = 'lorem ipsum';
+let string3 = 'javascript is cool';
+
+console.log(string1.toUpperCase());
+console.log(string2.toUpperCase());
+console.log(string3.toUpperCase());
