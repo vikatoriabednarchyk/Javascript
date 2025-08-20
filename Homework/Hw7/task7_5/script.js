598709cd01fbef70236c478ba90d363427bad73e
viktoriabednarchyk <viktoriabednarcik@gmail.com>
2025-08-20 22:57:50 +0300

    Створили task7_5.html та script.js та внесли зміни

---
+++ Homework/Hw7/task7_5/script.js
@@ -0,0 +1,23 @@
+function Client (id, name, surname, email, phone, order){
+    this.id = id;
+    this.name = name;
+    this.surname = surname;
+    this.email = email;
+    this.phone = phone;
+    this.order = order;
+}
+let clients = [
+    new Client(1, 'vika', 'bednarchyk', 'vika12@gmail.com', '+392930943', ['shampoo', 'shower gel' ]),
+    new Client(2, 'nastya', 'gyl', 'nastya11@gmail.com', '+330230930', ['powder', 'shadows']),
+    new Client(3, 'natalya', 'kachmaryk', 'natalya11@gmail.com', '+30940392490', ['nail', 'brush']),
+    new Client(4, 'lera', 'kindzerska', 'lera11@gmail.com', '+349302039', ['shower gel', 'nail', 'brush']),
+    new Client(5, 'roman', 'kalnytskiy', 'roman11@gmail.com', '+3040349039', ['book']),
+    new Client(6, 'halya', 'ahdryhiv', 'halya11@gmail.com', '+3040934933', ['tea', 'coffe', 'lush', 'shampoo']),
+    new Client(7, 'vasylyna', 'figel', 'vasylyna11@gmail.com', '+344903043', ['powder', 'lush']),
+    new Client(8, 'julia', 'trembach', 'julia11@gmail.com', '+3040434930', ['spf', 'toner', 'tea']),
+    new Client(9, 'oksana', 'bila', 'oksana11@gmail.com', '+44033980430', ['cream', 'soap']),
+    new Client(10, 'vika', 'litvinova', 'vika12@gmail.com', '+4934983293', ['shadows', 'lush', 'shower gel'])
+];
+
+let sortedOrders = clients.sort((u1, u2) => u1.order.length - u2.order.length);
+console.log(sortedOrders);
